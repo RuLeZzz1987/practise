c8e551da924950d4158d6ebe7e1d16246310270d
Alex Duka <aduka@alteryx.com>
2020-04-11 13:58:23 +0100

    Updates

---
+++ precache-manifest.24c4d706e220ea26e008ac777eb6f1c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb2b254c2c1e193635e41b62feb8675",
+    "url": "/practise/index.html"
+  },
+  {
+    "revision": "d5b45b66eff643aeab5c",
+    "url": "/practise/static/js/2.ebdb3e9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practise/static/js/2.ebdb3e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85d5e9a607c3bf738b3",
+    "url": "/practise/static/js/main.f76a9ee8.chunk.js"
+  },
+  {
+    "revision": "361937e8bb184af8eda0",
+    "url": "/practise/static/js/runtime-main.c5216479.js"
+  }
+]);
